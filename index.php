e3cd3a4aa367dfdd8d32bfca685b6dc6401b2dca
SimonSiksta420 <simon.siksta@student.ossp.cz>
2020-09-08 15:16:59 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prodej auta</title>
+</head>
+<body>
+    <h1> Prodejte svoje auto </h1>
+    Typ vozidla: 
+    
+    <select id="vehicle-type"> 
+     <option> Osobní </option>
+     <option> Nákladní </option>
+     <option> Karavan </option>
+     <option> Autobus </option>
+    </select> 
+
+<p> <form> 
+      <label for="spz"> SPZ: </label>
+      <input type="text" id="spz" name="spz"> <br>
+      <p> <input type="submit" value="submit"> </p>
+    </form> </p>
+    
+</body>
+</html>
