41bcd7f41aa4d9dc79810d07790ed01a373363b1
SimonSiksta420 <simon.siksta@student.ossp.cz>
2020-09-08 16:01:57 +0200

    Hotový kod
    Php upraveno změněno na for reach

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+$submit = filter_input(INPUT_POST, 'submit')
+$cars = filter_input(INPUT_POST, 'cars')
+$spz = filter_input(INPUT_POST, 'spz')
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,20 +12,35 @@
 </head>
 <body>
     <h1> Prodejte svoje auto </h1>
-    Typ vozidla: 
     
-    <select id="vehicle-type"> 
+    <label for="vehicle-type"> Typ vozidla </label>
+
+    <?php
+    if(isset($submit)) { ?>
+
+    <h2> Vozidlo prodáno <h2>
+    <p> Prodal jste <p>     
+        
+     <p> Auto: <?= $cars ?> </p>
+     <p> Spz: <?= $spz ?> </p>
+
+     <?php } else { ?>
+
+    <form action="index.php" method="post">
+
+    <select name="cars" id="vehicle-type"> 
      <option> Osobní </option>
      <option> Nákladní </option>
      <option> Karavan </option>
      <option> Autobus </option>
-    </select> 
+    </select>
 
-<p> <form> 
       <label for="spz"> SPZ: </label>
       <input type="text" id="spz" name="spz"> <br>
       <p> <input type="submit" value="submit"> </p>
+
     </form> </p>
-    
+        
+        <?php } ?>
 </body>
 </html>
